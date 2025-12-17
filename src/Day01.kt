7487c5a3e1bdadc836ccc610abbf7f6e5c16b621
Tim Sheppard <tim.sheppard@tradebyte.org>
2025-12-17 16:20:16 +0000

    Day 1, part 1 tentative solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        fun rotate(move: String, currentPos: Int): Int {
+            println("move: $move, pos: $currentPos")
+            // Get direction and size of move
+            val direction = if (move[0] == 'R') 1 else -1
+            // Only need last two digits as each 100 is a full rotation
+            val amountStr = move.substring(1)
+            val amount = if (amountStr.length <=2) amountStr.toInt() else amountStr.substring(amountStr.length - 2).toInt()
+
+            println("From move '${move}', direction: ${direction}, amount: ${amount}")
+            val newPos = currentPos + (direction * amount)
+            return when {
+                newPos in 0..99 -> newPos
+                newPos > 99 -> newPos - 100
+                else -> newPos + 100
+            }
+        }
+
+        val startPos = 50
+        val positions = MutableList(input.size){-1}
+        input.forEachIndexed { index, s ->
+            val currentPos = if (index == 0) startPos else positions[index - 1]
+            positions[index] = rotate(s, currentPos)
+        }
+
+        return positions.count { it == 0 }
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +32,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("R50")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 0)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
